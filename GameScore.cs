da1fd274e02a7020cef6d2c85341d243487a79eb
unknown <ivana.trajanovska@yahoo.com>
2014-05-11 18:23:02 +0200

    Prva Zadaca

---
+++ GameScore.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace domasno
+{
+    public partial class GameScore : Form
+    {
+        string text;
+        public GameScore()
+        {
+            InitializeComponent();
+            lblText.Text = string.Format("{0,-15}{1,-15}{2,-15}", "Name", "Score", "Best Word");
+            this.Location = new Point(250, 170);
+        }
+
+        public void AddScores(string s)
+        {
+            lblText.Text = string.Format("{0,-15}{1,-15}{2,-15}", "Name", "Score", "Best Word");
+            text = s;
+            lblText.Text += text;
+        }
+        private void GameScore_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
